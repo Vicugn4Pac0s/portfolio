c1068b4969f6919cb852bcba1793efdb4794f433
YOSHIKI <vicugnapacos.g@gmail.com>
2020-11-03 18:07:55 +0900

    ogp url設定

--- assets/parts/ogp.php
+++ assets/parts/ogp.php
@@ -9,8 +9,8 @@ function ogpSouceCode($name)
     ?>
     <meta property="og:title" content="<?php echo $pageName ?>Yoshiki's portfolio site">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+    <meta property="og:url" content="https://yoshikieee.com/">
+    <meta property="og:image" content="https://yoshikieee.com/images/ogp.png">
     <meta property="og:site_name" content="Yoshiki's portfolio site">
     <meta property="og:description" content="YOSHIKIのポートフォリオサイト。独学でWebサイト制作を学び、Web制作会社に入社、数々の大手企業のWebサイト制作に携わってきました。煩わしいこと・めんどくさいことが大嫌いすぎて、快適なPC環境を求めて日々模索中。">
 <?php
