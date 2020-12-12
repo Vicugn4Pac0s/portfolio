c98a3cf2a6e9a98ff4a27ec2a0f02fdd5b17ab98
YOSHIKI <vicugnapacos.g@gmail.com>
2020-12-12 19:12:52 +0900

    copyright 変更

--- assets/parts/footer.php
+++ assets/parts/footer.php
@@ -2,7 +2,7 @@
     <footer id="footer" class="footer">
         <div class="footer_inner l-inner">
             <p class="footer_copy">
-                <small>Copyright © 2020 YOSHIKI inc. All Rights Reserved.</small>
+                <small>Copyright © 2020 YOSHIKI. All Rights Reserved.</small>
             </p>
         </div>
     </footer>
