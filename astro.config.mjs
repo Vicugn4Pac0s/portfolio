57962d17e858a289afa9bae45ce38238704d1746
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2026-07-02 20:25:27 +0900

    Astroのエイリアス設定とReact統合を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@scripts': fileURLToPath(new URL('./src/scripts', import.meta.url)),
+      },
+    },
+  },
+});
