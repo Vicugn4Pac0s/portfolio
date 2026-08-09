7b5e1a610ab5f53df08f02a333abd145dbbfc050
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2026-08-09 16:10:08 +0900

    pagetopボタンを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     resolve: {
       alias: {
